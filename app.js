0811bea0b5252506298b0fae2f0cadf11afc7e22
kirti18patel <kirti18patel@yahoo.com>
2021-06-02 15:54:39 -0400

    html file created

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
+// console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+//   };
   
-  printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs);
+
+
+
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+
