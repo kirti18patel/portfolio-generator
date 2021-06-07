ea924cfa25ca1daad57696ad31253a14b7586a43
kirti18patel <kirti18patel@yahoo.com>
2021-06-07 10:56:02 -0400

    basic inquirer code added

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
-const fs = require("fs");
-const generatePage = require('./src/page-template.js');
-// console.log(profileDataArgs);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
+// const fs = require("fs");
+// const generatePage = require('./src/page-template.js');
+// // console.log(profileDataArgs);
 
-// const printProfileData = profileDataArr => {
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-//   };
+// // const printProfileData = profileDataArr => {
+// //     profileDataArr.forEach(profileItem => console.log(profileItem));
+// //   };
   
-//   printProfileData(profileDataArgs);
+// //   printProfileData(profileDataArgs);
 
 
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+//   fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
+
+const inquirer = require('inquirer');
+// console.log(inquirer);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
